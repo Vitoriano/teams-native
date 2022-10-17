bba572053d2da8aa5c2c2da68501d8df0a9de489
Vitoriano <vitorianoernandes2012@gmail.com>
2022-10-16 23:02:39 -0300

    Add biblioteca de icones

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,24 @@
-import { Container, Logo } from "./styles";
+
+import { Container, Logo, BackButton} from "./styles";
+import  CaretLeft  from 'react-native-vector-icons/AntDesign';
+//import { CaretLeft } from 'phosphor-react-native'; // todo: Corigir problema na instalação
 
 import logoImg from '@assets/logo.png';
 
-export function Header() {
+type Props = {
+  showBackButton?: boolean;
+}
+
+export function Header({ showBackButton = false}: Props) {
   return (
    <Container>
+    {
+      showBackButton && 
+      <BackButton>
+          <CaretLeft name="left" size={32} color="#fff"/>
+      </BackButton>
+    }
+   
     <Logo source={logoImg} />
    </Container>
   );
