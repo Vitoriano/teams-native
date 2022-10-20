dd79e692ad4668165ede1bca948511e32aad4ae0
Vitoriano <vitorianoernandes2012@gmail.com>
2022-10-19 22:48:55 -0300

    Adicionando o listempty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,17 @@
 import { useState } from 'react';
+import { FlatList } from 'react-native';
+
+import * as S from './styles';
 
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
-import * as S from './styles';
-import { FlatList } from 'react-native';
+import { ListEmpty } from '@components/ListEmpty';
+
 
 export default function Groups() {
 
-  const [groups, setGroups] = useState<string[]>(['Dev Na Rotina', 'Meu Time']);
+  const [groups, setGroups] = useState<string[]>([]);
 
   return (
     <S.Container>
@@ -23,6 +26,12 @@ export default function Groups() {
           renderItem={({ item }) => (
             <GroupCard  
               title={item}
+            />
+          )}
+          contentContainerStyle={groups.length === 0 && { flex: 1} }
+          ListEmptyComponent={() => (
+            <ListEmpty 
+              message="Que tal cadastra a primeira turma ?" 
             />
           )}
 
