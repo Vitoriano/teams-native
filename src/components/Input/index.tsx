c8a8c2465715276f72e823b81e64af96bcd1aa2a
Vitoriano <vitorianoernandes2012@gmail.com>
2022-10-22 18:32:41 -0300

    Component Input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,17 @@
+import { Container } from "./styles";
+import { useTheme } from "styled-components/native";
+
+import { TextInputProps } from "react-native";
+
+export function Input({...rest}: TextInputProps) {
+
+  const { COLORS } = useTheme();
+
+  return (
+    <Container
+      placeholderTextColor={COLORS.GRAY_300}
+      {...rest}
+    >
+    </Container>
+  )
+}
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -4,6 +4,7 @@ import { Header } from "@components/Header";
 import { Container, Content, IconItem } from "./styles";
 import { Highlight } from '@components/Highlight';
 import { Button } from '@components/Button';
+import { Input } from '@components/Input';
 
 export function NewGroup() {
   return (
@@ -20,8 +21,12 @@ export function NewGroup() {
           subtitle='Crie a turma para adicionar as pessoas'
         />
 
+        <Input 
+          placeholder='Nome da Turma'
+        />
         <Button 
           title="Criar"
+          style={{ marginTop: 20}}
         />
         
       </Content>
